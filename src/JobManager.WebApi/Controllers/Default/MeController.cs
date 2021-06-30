daad928d6921206a42c64bc633821fc5ab152c41
Leonardo Ferreira <leo.oliveira.eng@outlook.com>
2021-06-30 02:08:40 -0300

    project structure

---
+++ src/JobManager.WebApi/Controllers/Default/MeController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace JobManager.WebApi.Controllers.Default
+{
+    [ApiController, Route("api/[controller]")]
+    public class MeController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get() => Ok(new { name = "Job.Manager", version = "1.0.0" });
+    }
+}
